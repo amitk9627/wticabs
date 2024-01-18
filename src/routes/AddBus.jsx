a0a55c68bca89ea55ee4c3014dd3d4d90e1a5fbe
Amit Kumar <ramitgzp56@gmail.com>
2024-01-18 12:33:31 +0530

    nothing

--- src/routes/AddBus.jsx
+++ src/routes/AddBus.jsx
@@ -3,10 +3,10 @@ import {TextField} from '@mui/material'
 export const AddBus = () => {
   return (
     <div>
-      <TextField id="standard-basic" label="first" variant="standard" />
-      <TextField id="standard-basic" label="Std" variant="standard" />
-      <TextField id="standard-basic" label="Sfjvikr" variant="standard" />
-      <TextField id="standard-basic" label="Stvdnsl" variant="standard" />
+      <TextField id="standard-basc" label="first" variant="standard" />
+      <TextField id="standard-bsic" label="Std" variant="standard" />
+      <TextField id="standard-basi" label="Sfjvikr" variant="standard" />
+      <TextField id="standad-basic" label="Stvdnsl" variant="standard" />
     </div>
   )
 }
